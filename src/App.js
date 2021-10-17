4a1c7eadf879ee21acf56bea4ecc444c8016c9c8
saurabhkacholiya <kacholiyasaurabh@gmail.com>
2021-10-17 20:22:07 +0530

    feat: complete pokemon card design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import searchIcon from "./assets/search.svg";
+import filterIcon from "./assets/filterIcon.svg";
+
+function PokemonCard({ name, url }) {
+  const [pokeImg, setPokeImage] = useState([]);
+
+  useEffect(() => {
+    const splitData = url.split("/");
+    const id = splitData[splitData.length - 2];
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((res) => {
+        if (res.data && res.data.sprites)
+          setPokeImage(res.data.sprites.front_shiny);
+      })
+      .catch((err) => {
+        console.log("error in pokemon card", err);
+      });
+  }, []);
+
+  return (
+    <div key={name} className="pokemon_card">
+      <img alt={name} src={pokeImg} />
+      <div>{name}</div>
+    </div>
+  );
+}
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=100")
+      .then((res) => {
+        console.log("result is ", res.data);
+        if (res.data && res.data.results) {
+          setPokemonData(res.data.results);
+        }
+      })
+      .catch((err) => {
+        console.log("error in listing api", err);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img alt="search icon" src={searchIcon} />
+        <input
+          type="text"
+          className="input_search"
+          placeholder="Search.."
+        ></input>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {pokemonData.map((item) => (
+          <PokemonCard name={item.name} url={item.url} />
+        ))}
+      </div>
     </div>
   );
 }
